d08f06d7be58f0454922dbcc54f6560cc29ef1fb
Ravindra Birla <84989844+Birlaravi@users.noreply.github.com>
2025-01-19 01:18:34 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world! This is Ravindra solve your problem with me!!");
         
         Student sc = new Student();
         sc.display();
